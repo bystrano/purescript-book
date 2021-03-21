261e52a24bc09a2b138b364a2eea8123dd2834c4
Michel Bystranowski <bystrano@gmx.ch>
2021-03-21 15:45:19 +0100

    chapter 10 - parts 1 & 2

--- exercises/chapter10/test/MySolutions.js
+++ exercises/chapter10/test/MySolutions.js
@@ -1,3 +1,22 @@
 "use strict";
 
-// Note to reader: Add your solutions to this file
+exports.volumeFn = function (a, b, c) {
+  return a * b * c;
+};
+
+exports.volumeArrow = (a) => (b) => (c) => a * b * c;
+
+exports.cumulativeSumsComplex = (arr) => {
+  let sum = { real: 0, imag: 0 };
+  let sums = [];
+
+  arr.forEach(x => {
+    sum = {
+      real: sum.real + x.real,
+      imag: sum.imag + x.imag
+    };
+    sums.push(sum);
+  });
+
+  return sums;
+}
--- exercises/chapter10/test/MySolutions.purs
+++ exercises/chapter10/test/MySolutions.purs
@@ -2,4 +2,18 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+import Data.Function.Uncurried (Fn3)
+import Test.Examples (Complex)
+
+{- Calling javascript functions -}
+
+-- exercise 1
+foreign import volumeFn :: Fn3 Number Number Number Number
+
+-- exercise 2
+foreign import volumeArrow :: Number -> Number -> Number -> Number
+
+{- Passing simple types -}
+
+-- exercise 1
+foreign import cumulativeSumsComplex :: Array Complex -> Array Complex
