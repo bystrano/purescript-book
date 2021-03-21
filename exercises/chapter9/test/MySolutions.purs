c33762aef6c20fe1a85a3d19e60c29b1a5b00e3f
Michel Bystranowski <bystrano@gmx.ch>
2021-03-21 11:31:44 +0100

    chapter 9 - Part 1

--- exercises/chapter9/test/MySolutions.purs
+++ exercises/chapter9/test/MySolutions.purs
@@ -2,4 +2,33 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+import Data.Either (Either)
+import Data.Foldable (fold)
+import Data.String (length)
+import Data.Traversable (traverse)
+import Effect.Aff (Aff, Error, attempt)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readTextFile, writeTextFile)
+import Node.Path (FilePath)
+
+{- Asynchronous PureScript -}
+
+-- Exercise 1
+concatenateFiles :: String -> String -> String -> Aff Unit
+concatenateFiles inFile1 inFile2 outFile = do
+  inStr1 <- readTextFile UTF8 inFile1
+  inStr2 <- readTextFile UTF8 inFile2
+  outStr <- pure $ inStr1 <> inStr2
+  writeTextFile UTF8 outFile outStr
+
+-- exercise 2
+concatenateMany :: Array String -> String -> Aff Unit
+concatenateMany files outFile = do
+  contents <- traverse (readTextFile UTF8) files
+  writeTextFile UTF8 outFile (fold contents)
+
+-- exercise 3
+countCharacters :: FilePath -> Aff (Either Error Int)
+countCharacters file = attempt do
+  content <- readTextFile UTF8 file
+  pure $ length content
