2fa267bf60bc99a2693f4abd156686f33d8b7977
Michel Bystranowski <bystrano@gmx.ch>
2021-04-06 11:28:05 +0200

    chapter 12 - part 1

--- exercises/chapter12/src/Example/Shapes.purs
+++ exercises/chapter12/src/Example/Shapes.purs
@@ -2,11 +2,13 @@ module Example.Shapes where
 
 import Prelude
 
+import Data.Array (head, tail, zip, (..))
+import Data.Foldable (sequence_)
+import Data.Int (toNumber)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Data.Maybe (Maybe(..))
-import Graphics.Canvas (closePath, lineTo, moveTo, fillPath,
-                        setFillStyle, arc, rect, getContext2D,
-                        getCanvasElementById)
+import Graphics.Canvas (Context2D, arc, closePath, fillPath, getCanvasElementById, getContext2D, lineTo, moveTo, rect, setFillStyle, setStrokeStyle, strokePath)
 import Math as Math
 import Partial.Unsafe (unsafePartial)
 
@@ -27,8 +29,9 @@ main = void $ unsafePartial do
   ctx <- getContext2D canvas
 
   setFillStyle ctx "#00F"
+  setStrokeStyle ctx "#00F"
 
-  fillPath ctx $ rect ctx $ translate (-200.0) (-200.0)
+  strokePath ctx $ rect ctx $ translate (-200.0) (-200.0)
     { x: 250.0
     , y: 250.0
     , width: 100.0
@@ -52,3 +55,59 @@ main = void $ unsafePartial do
     lineTo ctx 260.0 340.0
     lineTo ctx 340.0 340.0
     closePath ctx
+
+  -- exercise 2
+  strokePath ctx $ do
+    rect ctx { x: 250.0
+             , y: 250.0
+             , width: 100.0
+             , height: 100.0
+             }
+    rect ctx { x: 250.0
+             , y: 150.0
+             , width: 50.0
+             , height: 50.0
+             }
+
+  setStrokeStyle ctx "#F00"
+  strokePath ctx $ do
+    moveTo ctx  30.0 300.0
+    lineTo ctx 100.0 300.0
+    arc ctx { x: 100.0
+            , y: 300.0
+            , radius: 70.0
+            , start: Math.tau / 4.0
+            , end: Math.tau / 2.0
+            }
+
+  renderFunction ctx (\f -> { x: 600.0 - 500.0 * f, y: 600.0 - 500.0 * f * f }) 100
+
+-- exercise 3
+type Point = { x :: Number, y :: Number }
+
+rotate :: forall a. Array a -> Array a
+rotate xs = case head xs of
+  Just h -> (fromMaybe [] $ tail xs) <> [ h ]
+  _      -> []
+
+toPairs :: forall a. Array a -> Array (Tuple a a)
+toPairs xs = zip xs $ rotate xs
+
+drawLine :: Context2D -> Tuple Point Point -> Effect Unit
+drawLine ctx (Tuple p1 p2) =
+  strokePath ctx $ do
+    moveTo ctx p1.x p1.y
+    lineTo ctx p2.x p2.y
+    closePath ctx
+
+renderPath :: Context2D -> Array Point -> Effect Unit
+renderPath ctx xs = sequence_ $ drawLine ctx <$> toPairs xs
+
+sample :: (Number -> Point) -> Int -> Array Point
+sample f n = f
+             <$> (\m -> m / (toNumber n))
+             <$> toNumber
+             <$> 0 .. (n - 1)
+
+renderFunction :: Context2D -> (Number -> Point) -> Int -> Effect Unit
+renderFunction ctx f n = renderPath ctx $ sample f n
